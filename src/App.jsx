e957e15b7c43d81495d97d69ff7d401322b0c773
Perry <perrymacdougall@gmail.com>
2019-07-02 05:31:50 +0000

    created initial components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,45 @@
 import React, {Component} from 'react';
+// import messages from "../messages.js";
+
+class Navbar extends Component {
+  render() {
+    return (
+      <nav className='navbar'>
+        <a href='/' className='navbar-brand'>Chatty</a>
+      </nav>
+    );
+  }
+}
+
+class Main extends Component {
+  render() {
+    return (
+      <main className='messages'>
+
+      </main>
+    );
+  }
+}
+
+class Footer extends Component {
+  render() {
+    return (
+      <footer className='chatbar'>
+        <input className='chatbar-username' placeholder='Your name here' />
+        <input className='chatbar-message' placeholder='Type your heart out' />
+      </footer>
+    );
+  }
+}
 
 class App extends Component {
   render() {
     return (
-      <h1>Hello React :)</h1>
+      <div className='container'>
+        <Navbar />
+        <Main />
+        <Footer />
+      </div>
     );
   }
 }
