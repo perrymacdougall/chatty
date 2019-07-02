175881897f43b129999f10e90f1c9dfc13b4c56a
Perry <perrymacdougall@gmail.com>
2019-07-02 17:15:55 +0000

    passed currentUser value to input element as a prop

--- messages.js
+++ messages.js
@@ -1,35 +1,15 @@
-[
-  {
-    type: "incomingMessage",
-    content: "I won't be impressed with technology until I can download food.",
-    username: "Anonymous1"
-  },
-  {
-    type: "incomingNotification",
-    content: "Anonymous1 changed their name to nomnom",
-  },
-  {
-    type: "incomingMessage",
-    content: "I wouldn't want to download Kraft Dinner. I'd be scared of cheese packet loss.",
-    username: "Anonymous2"
-  },
-  {
-    type: "incomingMessage",
-    content: "...",
-    username: "nomnom"
-  },
-  {
-    type: "incomingMessage",
-    content: "I'd love to download a fried egg, but I'm afraid encryption would scramble it",
-    username: "Anonymous2"
-  },
-  {
-    type: "incomingMessage",
-    content: "This isn't funny. You're not funny",
-    username: "nomnom"
-  },
-  {
-    type: "incomingNotification",
-    content: "Anonymous2 changed their name to NotFunny"
-  }
-]
+const messages = {
+                  currentUser: {name: "Bob"}, // optional. if currentUser is not defined, it means the user is Anonymous
+                  messages: [
+                    {
+                      username: "Bob",
+                      content: "Has anyone seen my marbles?",
+                    },
+                    {
+                      username: "Anonymous",
+                      content: "No, I think you lost them. You lost your marbles Bob. You lost them for good."
+                    }
+                  ]
+}
+
+export default messages;
--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-// import messages from "../messages.js";
+import messages from "../messages.js";
 
 class Navbar extends Component {
   render() {
@@ -11,21 +11,46 @@ class Navbar extends Component {
   }
 }
 
+class Message extends Component {
+  constructor(props) {
+    super(props);
+    const { currentUser } = messages;
+    this.name = currentUser.name;
+    console.log(this.name);
+  }
+
+  render() {
+    return (
+      <div className='message'>
+        <span className='message-username'>{this.name}</span>
+        <span className='message-content'>{/*this.messages.messages.content*/}</span>
+      </div>
+    );
+  }
+}
+
 class Main extends Component {
   render() {
     return (
       <main className='messages'>
-
+        <Message />
       </main>
     );
   }
 }
 
 class Footer extends Component {
+  constructor(props) {
+    super(props);
+    const { currentUser } = messages;
+    this.name = currentUser.name;
+    console.log(this.name);
+  }
+
   render() {
     return (
       <footer className='chatbar'>
-        <input className='chatbar-username' placeholder='Your name here' />
+        <input className='chatbar-username' defaultValue={this.name} placeholder='Your name here' />
         <input className='chatbar-message' placeholder='Type your heart out' />
       </footer>
     );
@@ -33,14 +58,35 @@ class Footer extends Component {
 }
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loading: true,
+      messages
+    }
+  }
+
+  componentDidMount() {
+
+    setTimeout(() => {
+      this.setState({ loading: false });
+    }, 3000);
+  }
+
   render() {
-    return (
-      <div className='container'>
-        <Navbar />
-        <Main />
-        <Footer />
-      </div>
-    );
+
+    if (this.state.loading) {
+      return <h1>Loading...</h1>
+    } else {
+      return (
+        <div className='container'>
+          <Navbar />
+          <Main />
+          <Footer />
+        </div>
+      );
+    }
   }
 }
+
 export default App;
